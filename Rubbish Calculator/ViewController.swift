63fb009593efe0849c0b81293ca9c76fb6a0b05d
Chris Chow <chris.yo.chow@gmail.com>
2016-12-05 18:56:25 +0800

    Operator Button Modified

--- Rubbish Calculator/ViewController.swift
+++ Rubbish Calculator/ViewController.swift
@@ -69,6 +69,7 @@ class ViewController: UIViewController {
             leftStrValue = ""
             rightStrValue = ""
             resultStrVaule = ""
+            currentOperator = CalcOperator.Empty
         }
         
         runningNumber = ""
